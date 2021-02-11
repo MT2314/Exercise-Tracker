da5a535b97caa7c9cc65c9dee0f24dd7104df8e5
MT2314 <39105763+MT2314@users.noreply.github.com>
2021-02-10 23:34:04 -0500

    HTML routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,65 +1,21 @@
+
 // Dependencies
 const router = require("express").Router();
-const Workout = require("../models/workout.js");
+const path = require('path');
 
-// Get all the workouts 
-router.get("/api/workouts/", (req, res) => {
-    // Add a field of Total Duration
-    Workout.aggregate(
-        [{
-            $addFields: { totalDuration: { $sum: "$exercises.duration" } }
-        }]
-    )
-        .then((workoutDb) => {
-            res.json(workoutDb);
-        })
-        .catch((err) => {
-            res.status(400).json(err);
-        });
-});
-
-// Add exercise to the workout by the id
-router.put("/api/workouts/:id", (req, res) => {
-    Workout.findByIdAndUpdate(req.params.id,
-        {
-            $push: { exercises: { ...req.body } }
-        })
-
-        .then(workoutDb => {
-            res.json(workoutDb);
-        })
-
-        .catch(err => {
-            res.status(400).json(err)
-        })
+router.get("/", (req,res) =>{
+  res.sendFile(path.join(__dirname, "../public/index.html"))
 })
 
-// Create a workout
-router.post("/api/workouts/", (req, res) => {
-    Workout.create(req.body)
-        .then(workoutDb => {
-            res.json(workoutDb);
-        })
-        .catch(err => {
-            res.status(400).json(err)
-        })
+
+router.get("/exercise", (req,res) =>{
+  res.sendFile(path.join(__dirname, "../public/exercise.html"))
 })
 
-// Get last 7 workouts added
-router.get("/api/workouts/range", (req, res) => {
-    Workout.aggregate(
-        [{
-            $addFields: { totalDuration: { $sum: "$exercises.duration" } }
-        }]
-    )
-        .sort({ day: -1 })
-        .limit(7)
-        .then(workoutDb => {
-            res.json(workoutDb.reverse()); 
-        })
-        .catch(err => {
-            res.status(400).json(err)
-        })
+
+router.get("/stats", (req,res) =>{
+  res.sendFile(path.join(__dirname, "../public/stats.html"))
 })
+
 
 module.exports = router;
