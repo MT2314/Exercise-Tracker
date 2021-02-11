a315189c470d50ba8bb8ffe493e09042432d40c3
MT2314 <39105763+MT2314@users.noreply.github.com>
2021-02-10 23:53:57 -0500

    change folder structure

---
+++ server.js
@@ -0,0 +1,36 @@
+// Dependencies
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+
+// Port number
+const PORT = process.env.PORT || 8080;
+
+// Express
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(
+  process.env.MONGDB_URI || 'mongodb://localhost/workout',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  }
+);
+
+// Routes
+app.use(require("./routes/api-routes.js"))
+app.use(require("./routes/html-routes.js"))
+
+// Start app
+app.listen(PORT, () => {
+  console.log(`App running on port http://localhost:${PORT}/`);
+});
